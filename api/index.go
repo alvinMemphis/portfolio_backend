0f9a9bd7071241e74719fe18585009bee35399e1
alvinmemphis <alvinmemphis@github.com>
2025-09-25 21:09:45 +0300

    rm:default prefxi

--- api/index.go
+++ api/index.go
@@ -10,7 +10,6 @@ func setupRouter() *gin.Engine {
 	// Create a Gin router with default middleware (logger and recovery)
 	r := gin.Default()
 	// Define a simple GET endpoint
-	r.Group("/")
 	r.GET("/api/ping", func(c *gin.Context) {
 		// Return JSON response
 		c.JSON(http.StatusOK, gin.H{
